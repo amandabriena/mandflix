a045d7d2cdc18039f13003b41c351d07c53f1ce5
amandabriena <amanda.briena@gmail.com>
2020-07-29 00:12:48 -0300

    Parte de cadastros

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.png'
 import './Menu.css';
+import { Link } from 'react-router-dom';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu" >
-            <a href="/">
+            <Link to="/">
            <img className="Logo" src={Logo} alt="MANDFLIX logo" />
-           </a>
+           </Link>
 
-           <Button as="a" className="ButtonLink" href="/">
+           <Button as="Link" className="ButtonLink" to="/cadastro/video">
             Novo Vídeo
            </Button>
         </nav>
