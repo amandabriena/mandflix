1888c95a8bb13293ce0b78ab8895b05095c55be4
amandabriena <amanda.briena@gmail.com>
2020-07-29 00:45:44 -0300

    Ajustes

--- src/components/Menu/component/ButtonLink/index.js
+++ src/components/Menu/component/ButtonLink/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-
 function ButtonLink(props) {
-    return (
-        <a href={props.href} className={props.className}>
-            Novo Vídeo
-        </a>
-    );
+  // props => { className: "o que alguém passar", href: "/" }
+  return (
+    <a href={props.href} className={props.className}>
+      {props.children}
+    </a>
+  );
 }
 
 export default ButtonLink;
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,19 +1,18 @@
+  
 import React from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-function CadastroVideo(){
-    return(
+function CadastroVideo() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Video</h1>
 
-        <PageDefault>
-            <h1>Cadastro de Video</h1>
-            <Link to="/cadastro/categoria">
-                Cadastrar Categoria
-            </Link>
-        </PageDefault>
-        
-        )
-    
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroVideo;
